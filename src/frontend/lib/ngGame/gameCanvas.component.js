3b03679725f13b0ceb1339e44e8b2c739387adc2
Mariano Eiberman <mariano.eiberman@gmail.com>
2017-01-04 01:48:53 -0300

    Try to start converting all to angular component. TODO: request animation frame with components

--- src/frontend/lib/ngGame/gameCanvas.component.js
+++ src/frontend/lib/ngGame/gameCanvas.component.js
@@ -1,36 +1,59 @@
+window.requestAnimFrame = (function(){
+    return  function(/* function */ callback, /* DOMElement */ element){
+        window.setTimeout(callback, 1000 / 60);
+    };
+})();
+
 var app = angular.module('game', ['ngCookies']);
 app.factory('spritemanager', function(name1){
     this.name = name1;
     return this.name;
 });
 
-app.component('gamecreator', {
-    template: '<gamecanvas></gamecanvas>',
+app.component('gameCreator', {
+    template: '<game_canvas></game_canvas>',
     controller: ['$cookies', function($cookie, spritemanager){
-        this.config = {'id':"canvas1", 'width':screen.width, 'height':'500px'}
+        this.config = {'id':"canvas1", 'width':screen.width, 'height':'200px'}
         this.$onInit = function(){
-console.log($ctrl); //sss
+            console.log(this); //sss
         }
     }]
 });
 
 
-app.component('gamecanvas', {
-    template: '<canvas id="{{$ctrl.config.id}}" width="{{$ctrl.config.width}}" height="{{$ctrl.config.height}}"></canvas>',
-    controller: ['$cookies', function($cookie, spritemanager){
+app.component('gameCanvas', {
+    template: '<canvas id="{{$ctrl.config.id}}" width="{{$ctrl.config.width}}" height="{{$ctrl.config.height}}"></canvas>' +
+    '<button ng-click="$ctrl.drawRectangle()">Dibujar</button>',
+    controller: ['$cookies', '$timeout', function($cookie, $timeout, spritemanager){
         this.config = {'id':"canvas1", 'width':screen.width, 'height':'500px'}
+        this.ctx = null;
+        this.rect = {x:15, y:15};
         this.$onInit = function(){
-            this.container = document.getElementById($ctrl.config.id);
-            console.log(containerId, this.container);
-            this.ctx = this.container.getContext('2d');
+            var self = this;
+            $timeout(function(){
+                self.container = document.getElementById(self.config.id);
+                self.ctx = self.container.getContext('2d');
+                self.drawRectangle();
+            },100);
             return this;
+        }
+        this.drawRectangle = function(){
+            console.log(this);
+            this.ctx.beginPath();
+            this.ctx.rect(this.rect.x, this.rect.y, 70, 70);
+            this.ctx.strokeStyle = '#000';
+            this.ctx.stroke();
+            this.ctx.font = '10px Arial';
+            this.ctx.strokeText('SQUARE', this.rect.x, this.rect.y + 80);
+            this.ctx.closePath();
+            requestAnimFrame( this.drawRectangle , this);
         }
     }]
 });
 
 /**
  * TODO
- * 
+ *
  * Crear componente gameCanvas
  *  en gameCanvas tengo que tener los metodos que tiene canvas
  *
@@ -41,5 +64,5 @@ app.component('gamecanvas', {
  *    <gameCreator>
  *        <gameCanvas></gameCanvas>
  *    </gameCreator>
- * 
+ *
  */
