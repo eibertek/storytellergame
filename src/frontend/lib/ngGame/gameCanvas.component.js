31ed756f440cc57c1d6a8452942dff8850922840
Mariano Eiberman <mariano.eiberman@gmail.com>
2017-01-04 10:10:15 -0300

    add dinamic animation

--- src/frontend/lib/ngGame/gameCanvas.component.js
+++ src/frontend/lib/ngGame/gameCanvas.component.js
@@ -1,8 +1,3 @@
-window.requestAnimFrame = (function(){
-    return  function(/* function */ callback, /* DOMElement */ element){
-        window.setTimeout(callback, 1000 / 60);
-    };
-})();
 
 var app = angular.module('game', ['ngCookies']);
 app.factory('spritemanager', function(name1){
@@ -27,26 +22,51 @@ app.component('gameCanvas', {
     controller: ['$cookies', '$timeout', function($cookie, $timeout, spritemanager){
         this.config = {'id':"canvas1", 'width':screen.width, 'height':'500px'}
         this.ctx = null;
-        this.rect = {x:15, y:15};
+        this.rect = {x:5, y:5, width: 90, height: 15, orientation:1, velocity:1};
+        this.objects = [];
         this.$onInit = function(){
             var self = this;
             $timeout(function(){
                 self.container = document.getElementById(self.config.id);
                 self.ctx = self.container.getContext('2d');
-                self.drawRectangle();
+                for(var i = 1; i< 240; i++)
+                {
+                    var copy = angular.copy(self.rect);
+                    copy.velocity=10+Math.random()*10;
+                    copy.y = copy.height*i + 15*i;
+                    self.objects.push(copy);
+                }
+                self.draw();
             },100);
             return this;
         }
-        this.drawRectangle = function(){
-            console.log(this);
+        this.draw = function(){
+            this.clearAll();
+            var self = this;
+            this.objects.forEach(function(object1){
+                self.drawRectangle(object1);
+                console.log(object1.x , object1.x + object1.width, self.config.width);
+                if(object1.x < 0 || object1.x + object1.width > self.config.width )
+                    object1.orientation*= -1;
+                object1.x+=object1.velocity*object1.orientation;
+            });
+            $timeout(function(){
+                self.draw();
+            }, 1);
+        }
+
+        this.clearAll = function() {
+            this.ctx.clearRect(0, 0, this.container.width, this.container.height);
+        }
+
+        this.drawRectangle = function(obj1){
             this.ctx.beginPath();
-            this.ctx.rect(this.rect.x, this.rect.y, 70, 70);
+            this.ctx.rect(obj1.x, obj1.y, obj1.width, obj1.height);
             this.ctx.strokeStyle = '#000';
             this.ctx.stroke();
             this.ctx.font = '10px Arial';
-            this.ctx.strokeText('SQUARE', this.rect.x, this.rect.y + 80);
+            this.ctx.strokeText('SQUARE', obj1.x, obj1.y + obj1.height+10);
             this.ctx.closePath();
-            requestAnimFrame( this.drawRectangle , this);
         }
     }]
 });
